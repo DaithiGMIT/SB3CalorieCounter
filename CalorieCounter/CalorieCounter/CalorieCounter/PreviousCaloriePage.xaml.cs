3517c7eaed05a08ce58e4cb7ba8c88615fa61f5a
DaithiGMIT <80974446+FionnMacSweeney@users.noreply.github.com>
2022-04-04 19:10:39 +0100

    Fionn Code, close #4

---
+++ CalorieCounter/CalorieCounter/CalorieCounter/PreviousCaloriePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalorieCounter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PreviousCaloriePage : ContentPage
+    {
+        public PreviousCaloriePage()
+        {
+            InitializeComponent();
+            LblDisplay.Text = "Daily Calories Total = " + Profile.dailyCals;
+        }
+
+        private void ShowPreviousDay_Clicked(object sender, EventArgs e)
+        {
+            Profile.displayYesterdayCals();
+        }
+
+        private void ShowPreviousWeek_Clicked(object sender, EventArgs e)
+        {
+            LblDisplay.Text = "Weekly Calories Total = " + Profile.weeklyCals;
+        }
+
+        private void SearchDate_Clicked(object sender, EventArgs e)
+        {
+            LblDisplay.Text = Profile.displayDateCals(EntrySearchDate.Text);
+        }
+
+        private async void MainPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
