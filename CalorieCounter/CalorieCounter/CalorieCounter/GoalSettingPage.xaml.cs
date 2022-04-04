c1690db7bc244cae7d00560b7f8ff861a98c3041
DaithiGMIT <98349443+PedroFGMIT@users.noreply.github.com>
2022-04-04 19:08:33 +0100

    Pedro Code, Closes #2

---
+++ CalorieCounter/CalorieCounter/CalorieCounter/GoalSettingPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalorieCounter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GoalSettingPage : ContentPage
+    {
+        public GoalSettingPage()
+        {
+            InitializeComponent();
+            LblDailyGoal.Text = "Current Daily Goal: " + Profile.dailyGoals;
+            LblWeeklyGoal.Text = "Current Weekly Goal: " + Profile.weeklyGoals;
+        }
+
+        private void DailyGoal_Clicked(object sender, EventArgs e)
+        {
+            Profile.updateGoals(1, EntryGoalSet.Text);
+            LblDailyGoal.Text = "Current Daily Goal: " + Profile.dailyGoals;
+        }
+
+        private void WeeklyGoal_Clicked(object sender, EventArgs e)
+        {
+            Profile.updateGoals(2, EntryGoalSet.Text);
+            LblWeeklyGoal.Text = "Current Weekly Goal: " + Profile.weeklyGoals;
+        }
+
+        private async void MainPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
